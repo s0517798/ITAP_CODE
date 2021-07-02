b717fecc31fc7512f398cda04025959dc2c007e7
LeeBV28 <84066230+LeeBV28@users.noreply.github.com>
2021-07-02 12:43:18 +0700

    delet

---
+++ js/header.js
@@ -0,0 +1,11 @@
+var t = document.getElementById("header")
+t.innerHTML="<div class=\"container\">\n" +
+    "    <div class=\"row\">\n" +
+    "        <img class=\"img-responsive\" src=\"images/lebv.jpg\"/>\n" +
+    "        <div class=\"conten top\">\n" +
+    "            <h1>Tôi Chẳng Có Gì Ngoài Một Tâm Hồn Đẹp</h1>\n" +
+    "            <p>Kết nối với tôi qua MXH chúng ta cùng chia sẻ với nhau nhiều hơn cho cuộc sống này thêm thú vị nhé!</p>\n" +
+    "            <hr/>\n" +
+    "        </div>\n" +
+    "    </div>\n" +
+    "  </div>":
